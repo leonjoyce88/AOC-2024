a8b45c2b36e1aeba26b0783445de146ace238f97
Leon Joyce <ljoyce2@sheffield.ac.uk>
2024-12-01 07:56:44 +0000

    setup and day1

---
+++ 01/01.py
@@ -0,0 +1,20 @@
+input = open("input.txt","r")
+
+
+total = 0
+left = []
+right = []
+
+for line in input:
+    a,b = line.split()
+    left.append(a)
+    right.append(b)
+
+left= sorted(left)
+right= sorted(right)
+
+for (a,b) in zip(left,right):
+    print(a,b)
+    total += abs(int(a)-int(b))
+
+print(total)
---
+++ 01/01p2.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+input = open("input.txt","r")
+
+
+total = 0
+left = []
+
+counts = defaultdict(int)
+
+for line in input:
+    a,b = line.split()
+    left.append(int(a))
+    counts[int(b)] += 1
+
+for num in left:
+    total += num * counts[num]
+
+
+print(total)
